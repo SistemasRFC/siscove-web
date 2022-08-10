588240804b5dae3788f412df143ea5aeeed71d63
Luis Gustavo Sousa Carneiro <lluis.gusttavosc@gmail.com>
2022-08-10 14:14:44 -0300

    inicio

---
+++ index.js
@@ -0,0 +1,27 @@
+$(function(){
+    $("#btnLogar").click(function(){
+        swal("Aguarde!");
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:8080/usuario/validar",
+            data: JSON.stringify({
+                nomeLogin: $("#nomeUsuario").val(),
+                txtSenha: $("#txtSenha").val()
+            }),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function(data){
+                console.log(data);
+                if (data.retorno){
+                    swal("", "Usuário confirmado!!!", "success");
+                    window.location.href='MenuPrincipal/menuPrincipal.html'
+                }else{
+                    swal("", "Usuário não confirmado!!!", "error"); 
+                }
+            },
+            error: function(err) {
+                    swal("", "Usuário não confirmado!!!", "error"); 
+                }
+            });
+        });
+    })
