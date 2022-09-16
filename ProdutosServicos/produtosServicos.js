fabc8b63e704fedf2f772d7f29205107c2882d27
Luis Gustavo Sousa Carneiro <lluis.gusttavosc@gmail.com>
2022-09-16 17:21:04 -0300

    Alteração no pacote Produto

--- ProdutosServicos/produtosServicos.js
+++ ProdutosServicos/produtosServicos.js
@@ -1,20 +1,33 @@
-$(document).ready(function () {
-    getListarProdutos();
 
-    $("#btnNovo").click(function () {
-    })
+$(document).ready(function () {
 });
+$(function () {
+    $("#btnNovo").click(function () {
+        
+        
+    })
+})
+$(function () {
+    $("#btnProcurar").click(function () {
+        getListarProdutos();
+    })
+})
+
+var dadosRetorno;
 
 function getListarProdutos() {
     $.ajax({
-        type: "GET",
-        url: "http://localhost:8080/cadastoprodutos/listar",
+        type: "POST",
+        url: "http://localhost:8080/produtos/listar/byTermo",
         beforeSend: function (xhr) {
             xhr.setRequestHeader('Authorization', localStorage.getItem("token"));
         },
+        data: $("#Termo").val(),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
         success: function (data) {
             dadosRetorno = data.objeto;
-            montaTabela(data.objeto);
+            montaTabelaProdutos();
         },
         error: (err) => {
             swal("", "Erro!!!", "error");
@@ -22,29 +35,35 @@ function getListarProdutos() {
     });
 }
 
-function montaTabela(dados) {
+function montaTabelaProdutos(){
+    var dados = dadosRetorno;
     var tabela = '';
-    tabela += '<table class="table table-hover table-striped table-bordered table-white" id="tabelaProduto">';
+    tabela += '<table class="table table-hover table-striped table-bordered table-white" id="tabelaPerfil">';
     tabela += '<thead>';
     tabela += '    <tr align="center">';
-    tabela += '        <td width="25%">Produto </td>';
+    tabela += '        <td width="25%">Descrição </td>';
+    tabela += '        <td width="25%">Marca</td>';
     tabela += '        <td width="25%">Ativo</td>';
-    tabela += '        <td width="25%">Cod Marca</td>';
+    tabela += '        <td width="25%">Açoes</td> ';                                                  
     tabela += '    </tr>';
     tabela += '</thead>';
     tabela += '<tbody id="corpoTabela">';
-    tabela += "</td>";
-    tabela += "</tr>";
-    
+
     for (var i in dados) {
         tabela += "<tr>";
         tabela += "<td width='25%'>" + dados[i].dscProduto + "</td>";
+        tabela += "<td width='25%'>" + dados[i].dscMarca + "</td>";
         tabela += "<td width='25%'>" + dados[i].indAtivo + "</td>";
-        tabela += "<td width='25%'>" + dados[i].codMarca + "</td>";
         tabela += "<td width='25%'  style='text-align:center;'>";
+        tabela += "    <a href='javascript:preencherCampos(" + i + ")'>";
+        tabela += "        <i class='fas  fa-pen'></i>";
+        tabela += "    </a>";
+        tabela += "    </a>";
+        tabela += "</td>";
         tabela += "</tr>";
     }
     tabela += '</tbody>';
     tabela += '</table>';
-        $("#tabelaProduto").html(tabela);
-    }
+    $("#tabelaProduto").html(tabela);
+    $("#tabelaPerfil").DataTable();
+}
